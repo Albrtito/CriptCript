af5c439fe887e142682ccfea0fee64cdd8a74500
Javier Martin <jmartinpizarro04@gmail.com>
2024-10-23 19:08:48 +0200

    Add logIn frontend  user logic

---
+++ frontend/scripts/loginsr.js
@@ -0,0 +1,19 @@
+export async function logInUser(){
+    const username = document.getElementById('user')
+    const password = document.getElementById('password')
+
+    const response = await fetch('http://localhost:5001/login_user', {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({"username": user, "password": password})
+    });
+
+    if (response.status === 422) {
+        localStorage.setItem('userLogged', username)
+        window.location.href = './challenges.html'
+    } else {
+        alert('User does not exist')
+    }
+}
