21cb37a7f4f40787ad8516255aa78364c18860bd
Javier Martin <jmartinpizarro04@gmail.com>
2024-10-23 19:57:10 +0200

    Add frontend for creating challenges

--- frontend/scripts/createChallenge.js
+++ frontend/scripts/createChallenge.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sendButton = document.getElementById('send-button')
-    sendButton.addEventListener('click', (event) => {
+    sendButton.addEventListener('click', async (event) => {
         event.preventDefault()
-        console.log('click')
+        const title = document.getElementById('title').value
+        const document = document.getElementById('document').value
+        const userToShare = document.getElementById('user').value
+        const userLogged = localStorage.getItem('userLogged')
+
+        if (title === '' || document === '' || userToShare === '' || userLogged === ''){
+            alert('There is something that cannot be empty... Or maybe you are not log in correctly?')
+        }
+
+        const response = await fetch('http://localhost:5001/create_challenge', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({"title": title, "document": document, "userLogged": userLogged, "userToShare": userToShare})
+        })
+
+        if (response.status === 201) {
+            alert('El challenge se ha creado correctamente')
+            window.location.hred='./challenges.html'
+        }
     })
 })
