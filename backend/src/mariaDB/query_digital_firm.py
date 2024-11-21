064ba85dd10aa5b55ada3ec98be67736466f7466
Javier Martin <jmartinpizarro04@gmail.com>
2024-11-21 08:44:09 +0100

    Add query for inserting keys for the digital firm

---
+++ backend/src/mariaDB/query_digital_firm.py
@@ -0,0 +1,34 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+import mysql.connector
+from src.mariaDB.connection import DATABASE_DIGITAL_SIGN_NAME, get_digital_sign_db_connection
+
+
+def insert_secure_keys(
+    username: str,
+    encrypted_private_key: bytes,
+    public_key: str
+) -> bool:
+    """
+    Inserts digital firm keys for each user registered
+    """
+    try:
+        connection = get_digital_sign_db_connection()
+        cursor = connection.cursor()
+
+        query = f"INSERT INTO {DATABASE_DIGITAL_SIGN_NAME}.secure_keys(username, encrypted_private_key, public_key) VALUES (%s, %s, %s)"
+        cursor.execute(
+            query, (username, encrypted_private_key, public_key)
+        )
+
+        connection.commit()
+
+    except mysql.connector.Error as e:
+        logging.debug("%s", e)
+        return False
+
+    except Exception as e:
+        logging.debug("%s", e)
+        return False
+    return True
