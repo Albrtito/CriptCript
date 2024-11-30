d0f5f1a75c4dfd938a4b0482f39d71f906e4102c
Javier Martin <jmartinpizarro04@gmail.com>
2024-11-30 20:44:26 +0100

    Create CertificateManage file

---
+++ backend/src/utils/certificate/CertificateManager.py
@@ -0,0 +1,6 @@
+class CertificateManager():
+    """
+    This class implements everything related with certificates (and its entities) for a detailed verification of the public key
+    """
+    def __init__(self) -> None:
+        pass
